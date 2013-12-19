78529f7db572ce4bfce9dcb0d4dd4cb9290898f2
Bishnu Bidari <bidaribishnu7@gmail.com>
2013-12-19 17:45:37 +0545

    ListView with custom Adapter to redesine the View

---
+++ ListViewCustom/gen/com/bajar/listviewcustom/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.bajar.listviewcustom;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ListViewCustom/src/com/bajar/listviewcustom/MainActivity.java
@@ -0,0 +1,27 @@
+package com.bajar.listviewcustom;
+
+import com.bajar.listviewcustom.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+	Item[] items= new Item[3];
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	
+		items[0] = new Item("Chair","GooD",R.drawable.mob1);
+		items[1] = new Item("Bed","Wrost", R.drawable.mob2);
+		items[2] = new Item("Book", "Fine", R.drawable.mob3);
+		
+		ListView lvItem = (ListView)findViewById(R.id.lvItems);
+		lvItem.setAdapter(new CustomListAdapter(getApplicationContext(), items));
+	
+	}
+
+}
