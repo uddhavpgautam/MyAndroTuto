20db255fc7f15ce821978025e4fd2144a76d5ecc
Bishnu Bidari <bidaribishnu7@gmail.com>
2013-12-20 12:33:10 +0545

    Autoscrollable text View

---
+++ NewsReader/src/com/bajra/newsreader/AutoScrollableTextView.java
@@ -0,0 +1,54 @@
+package com.bajra.newsreader;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.text.TextUtils.TruncateAt;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class AutoScrollableTextView extends TextView {
+
+	public AutoScrollableTextView(Context context, AttributeSet attrs,
+			int defStyle) {
+		super(context, attrs, defStyle);
+		setVariousSettings();
+
+	}
+
+	public AutoScrollableTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setVariousSettings();
+
+	}
+
+	public AutoScrollableTextView(Context context) {
+		super(context);
+		setVariousSettings();
+
+	}
+
+	private void setVariousSettings() {
+		setEllipsize(TruncateAt.MARQUEE);
+		setMarqueeRepeatLimit(-1);
+		setSingleLine();
+		setHorizontallyScrolling(true);
+	}
+
+	@Override
+	protected void onFocusChanged(boolean focused, int direction,
+			Rect previouslyFocusedRect) {
+		if (focused)
+			super.onFocusChanged(focused, direction, previouslyFocusedRect);
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean focused) {
+		if (focused)
+			super.onWindowFocusChanged(focused);
+	}
+
+	@Override
+	public boolean isFocused() {
+		return true;
+	}
+}
