6adddec622a76b2e6493b408295250e54dd3e93a
Bishnu Bidari <bidaribishnu7@gmail.com>
2013-12-20 17:04:48 +0545

    use of fragment with custom adapter for ListView

---
+++ NewsReader/src/com/bajra/newsreader/Sites.java
@@ -0,0 +1,24 @@
+package com.bajra.newsreader;
+
+public class Sites {
+String siteName;
+String siteUrl;
+public Sites(String siteName, String siteUrl){
+	this.siteName = siteName;
+	this.siteUrl = siteUrl;
+}
+
+public String getSiteName() {
+	return siteName;
+}
+public void setSiteName(String siteName) {
+	this.siteName = siteName;
+}
+public String getSiteUrl() {
+	return siteUrl;
+}
+public void setSiteUrl(String siteUrl) {
+	this.siteUrl = siteUrl;
+}
+
+}
